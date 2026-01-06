537b0d05e3fa786aa9c28fd5d3081fecaa425a8c
hari-krishnan427 <harikrishnanboopalan2@gmail.com>
2026-01-06 19:55:48 +0530

    CodeAlpha_network_sniffer.py

--- CodeAlpha_network_sniffer/CodeAlpha_network_sniffer.py
+++ CodeAlpha_network_sniffer/CodeAlpha_network_sniffer.py
@@ -1,10 +1,16 @@
 from scapy.all import sniff
 from scapy.layers.inet import IP, TCP, UDP, ICMP
+from datetime import datetime
+
+packet_count = 0
 
 def packet_analyzer(packet):
-    print("\n========== New Packet ==========")
+    global packet_count
 
     if IP in packet:
+        packet_count += 1
+        print("\n========== Packet", packet_count, "==========")
+        print("Time           :", datetime.now().strftime("%H:%M:%S"))
         print("Source IP      :", packet[IP].src)
         print("Destination IP :", packet[IP].dst)
 
@@ -14,7 +20,18 @@ def packet_analyzer(packet):
             print("Protocol       : UDP")
         elif ICMP in packet:
             print("Protocol       : ICMP")
+        else:
+            print("Protocol       : Other")
 
-print("Network Sniffer Started... Capturing 10 packets only")
-sniff(prn=packet_analyzer, store=False, count=10)
-print("Sniffing completed.")
+print("Network Sniffer Started...")
+print("Capturing packets for 1 minute...\n")
+
+sniff(
+    iface="Wi-Fi",      
+    prn=packet_analyzer,
+    store=False,
+    timeout=60
+)
+
+print("\nSniffing stopped.")
+print("Total packets captured:", packet_count)
